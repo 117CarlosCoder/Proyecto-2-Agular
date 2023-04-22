144f6c567b0bfeee5d29211e8538101bf38e612a
117CarlosCoder <carloslopez202031871@cunoc.edu.gt.com>
2023-04-22 14:10:52 -0600

    mas componentes para el medico

--- src/app/menus/menu-medico/menu-incio-medico/menu-incio-medico.component.ts
+++ src/app/menus/menu-medico/menu-incio-medico/menu-incio-medico.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-menu-incio-medico',
   templateUrl: './menu-incio-medico.component.html',
   styleUrls: ['./menu-incio-medico.component.css']
 })
-export class MenuIncioMedicoComponent {
+export class MenuIncioMedicoComponent implements  OnInit {
+  especialidad = {
+    costo:'',
+    nombre:'',
+    descripcion:''
+  };
+
+  constructor(private http: HttpClient, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.inicializarDatos();
+  }
+
+  inicializarDatos(): void {
+    this.http.get('http://localhost:8080/apirest_war_exploded/medicos/especialidad', { responseType: 'text'}).subscribe(
+      (respuesta: any) => {
+        console.log('respuesta de carga ' + respuesta);
+
+
+        if (respuesta == 'true') {
+          console.log('cambio de estado');
+          this.cambiarEstado();
+        }
+
+        if (respuesta == 'false') {
+          this.mostrarInicioEspecialidad = true;
+        }
+        // La respuesta del servidor se ha recibido correctamente
+        console.log('respuesta de carga ' + respuesta);
+      },
+      (error) => {
+        // Ha ocurrido un error al enviar la solicitud al servidor
+        console.error(error);
+      }
+    );
+  }
+
+  enviarEspecialidad() {
+    console.log(this.especialidad.nombre)
+    const jsonEspecialidad = this.especialidad;
+    console.log(jsonEspecialidad)
+    this.http.post('http://localhost:8080/apirest_war_exploded/medicos/especialidad', jsonEspecialidad, { responseType: 'text'}).subscribe(
+      (respuesta: any) => {
+        console.log(respuesta);
+
+
+        if (respuesta === 'true') {
+          console.log('cambio de estado');
+          this.cambiarEstado();
+
+        }
+        // La respuesta del servidor se ha recibido correctamente
+        console.log(respuesta);
+      },
+      (error) => {
+        // Ha ocurrido un error al enviar la solicitud al servidor
+        console.error(error);
+      }
+    );
+  }
+
+  mostrarNavbarMedico = false;
+  mostrarNavbarInico = true;
+  mostrarInicioEspecialidad = false;
+
+  cambiarEstado() {
+    this.mostrarNavbarInico = false;
+    this.mostrarNavbarMedico = true;
+  }
 
 }
